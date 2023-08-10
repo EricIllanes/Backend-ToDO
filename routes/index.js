e739afdd9df80f8cb1388abbc1599f4384616edc
EricIllanes <eric.illanes@organizame.cl>
2023-08-10 13:22:26 -0400

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 const { Router } = require("express");
 const userRoute= require("./users.js")
-const userTask= require("./tasks.js");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const router = Router();
 // Agregar todos los routers;
 router.use(userRoute);
-router.use(userTask)
 
 
 module.exports = router;
